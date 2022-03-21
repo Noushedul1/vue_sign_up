e8483811645d97687f976d859762eae45edce6d5
Noushedul1 <noushedakib@gmail.com>
2022-03-21 14:11:51 +0600

    working with vuex state

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,20 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    books: [
+      {id: 1,bookname: 'web design',writter: 'noushedul',price: 300},
+      {id: 2,bookname: 'web developmenr',writter: 'akib',price: 400},
+      {id: 3,bookname: 'front end',writter: 'farhad',price: 260},
+      {id: 4,bookname: 'back end',writter: 'nuhash',price: 500},
+      {id: 5,bookname: 'full stack',writter: 'joy',price: 799},
+    ],
+    players: [
+      {id:1,type: 'allrounder',playername: 'Shakib',run: 600},
+      {id:2,type: 'allrounder',playername: 'Mehedi',run: 599},
+      {id:1,type: 'allrounder',playername: 'Afif',run: 367},
+      {id:1,type: 'batsman',playername: 'Tamin',run: 834},
+      {id:1,type: 'keeper',playername: 'Mushfiq',run: 557},
+    ]
   },
   getters: {
   },
--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,12 +1,41 @@
 <template>
   <div>
       Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut quidem cupiditate nulla consectetur fugit! A, fuga modi quaerat, nobis totam vel assumenda quibusdam ipsa rerum molestias qui nam perspiciatis cum.
+      <div v-for="b in books" :key="b.id">
+        {{b.bookname}} || {{b.writter}} || {{b.price}}
+      </div>
+      <ul>
+        <li v-for="p in players" :key="p.id">
+          {{p.type}} || {{p.playername}} || {{p.run}}
+        </li>
+      </ul>
+      <hr>
+      <div v-for="pr in playersrun" :key="pr.id">
+        {{pr.run}}
+      </div>
   </div>
 </template>
 
 <script>
+import {books,players} from '../store/index';
 export default {
     name: 'ProductView',
+    computed: {
+      books(){
+        return this.$store.state.books;
+      },
+      players(){
+        return this.$store.state.players;
+      },
+      playersrun(){
+        const playersrun = this.$store.state.players.map((player)=>{
+          return {
+            run: player.run/2
+          }
+        });
+        return playersrun;
+      }
+    }
 }
 </script>
 
